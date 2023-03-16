3490b84a638c6efad6075d7681638671c500857b
Merna Islam <101946404+mernaislam@users.noreply.github.com>
2023-03-16 18:02:39 +0200

    test

---
+++ cmake-build-debug/CMakeFiles/sortlib.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sortlib.dir/sortlib.cpp.obj"
+  "CMakeFiles/sortlib.dir/sortlib.cpp.obj.d"
+  "libsortlib.dll.a"
+  "sortlib.exe"
+  "sortlib.exe.manifest"
+  "sortlib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sortlib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/sortlib.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for sortlib.
+# This may be replaced when dependencies are built.
