2793132c22cb81ac86caec63f75646f83889bfdf
Merna Islam <101946404+mernaislam@users.noreply.github.com>
2023-03-16 16:49:36 +0200

    Adding Merge Function

--- main.cpp
+++ main.cpp
@@ -4,14 +4,22 @@
 int main() {
 
     int sz;
-    std::cout<<"Enter the size of array::";
-    std::cin>>sz;
+    std::cout << "Enter the size of array:: ";
+    std::cin >> sz;
     int randArray[sz];
+    int randArray2[sz];
     sortlib::generate(randArray, sz);
+    sortlib::generate(randArray2, sz);
     sortlib::selection<int>(randArray, sz);
+    sortlib::mergeSort<int>(randArray2, 0, sz-1);
     for(int i = 0 ; i < sz ; i++){
-        std::cout << i+1 << " : "<<randArray[i] << '\n';
+        std::cout << i+1 << " : " << randArray[i] << '\n';
     }
+    std::cout << "\n Sorting using Merge Sort: \n";
+    for(int i = 0 ; i < sz ; i++){
+        std::cout << randArray2[i] << ' ';
+    }
+    std::cout << "\n";
 
     return 0;
 }
